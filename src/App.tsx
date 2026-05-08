d0aca78ecf5ce4cc7878ef89eeb4eeeb9c664852
Alice Tolosa <aliceoliveira81@outlook.com>
2026-05-08 16:50:28 -0300

    feat: componeten login com autenticação do usuario com AuthContext

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,41 @@
-import { BrowserRouter as browserRouter, Routes, Route, BrowserRouter } from 'react-router-dom'
-import Home from './pages/home/Home'
-import Navbar from './components/navbar/Navbar'
-import Footer from './components/footer/Footer'
-import Cadastro from './pages/cadastro/Cadastro';
-import Login from './pages/login/Login';
+import {
+  BrowserRouter as browserRouter,
+  Routes,
+  Route,
+  BrowserRouter,
+} from "react-router-dom";
+import Home from "./pages/home/Home";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./components/footer/Footer";
+import Cadastro from "./pages/cadastro/Cadastro";
+import Login from "./pages/login/Login";
+import { AuthProvider } from "./contexts/AuthContext";
 
 function App() {
- 
   return (
-  <> 
-    <BrowserRouter>                                             {/*contêiner que habilita o roteamento */}
-      <Navbar />
-
-      <div className='min-h-[80vh]'>
-        
-        <Routes>      
-          <Route path="/" element={<Login />} />{/* Define as rotas da aplicação */}                                            
-          <Route path="/login" element={<Login />} />{/* Define as rotas da aplicação */}  
-          <Route path="/home" element={<Home />} />            {/* Rota para a página Home */} 
-          <Route path="/cadastro" element={<Cadastro />} />            {/* Rota para a página Home */}
-          
-        </Routes>
-      </div>
-      
-      <Footer /> 
-    </BrowserRouter>
-  
-  </>
-  
+    <>
+      <AuthProvider>
+        <BrowserRouter>
+          {" "}
+          {/*contêiner que habilita o roteamento */}
+          <Navbar />
+          <div className="min-h-[80vh]">
+            <Routes>
+              <Route path="/" element={<Login />} />
+              {/* Define as rotas da aplicação */}
+              <Route path="/login" element={<Login />} />
+              {/* Define as rotas da aplicação */}
+              <Route path="/home" element={<Home />} />{" "}
+              {/* Rota para a página Home */}
+              <Route path="/cadastro" element={<Cadastro />} />{" "}
+              {/* Rota para a página Home */}
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+      </AuthProvider>
+    </>
   );
 }
- 
-export default App
 
-
-
- 
+export default App;
