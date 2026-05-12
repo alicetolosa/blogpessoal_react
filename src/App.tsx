f1791c0bf2dfe928b85d586bd0a1a90aa4cc27d7
Alice Tolosa <aliceoliveira81@outlook.com>
2026-05-11 22:05:52 -0300

    inclusão dos componetes:CardTemas e ListaTemas, com estilização e logica

--- src/App.tsx
+++ src/App.tsx
@@ -10,6 +10,9 @@ import Footer from "./components/footer/Footer";
 import Cadastro from "./pages/cadastro/Cadastro";
 import Login from "./pages/login/Login";
 import { AuthProvider } from "./contexts/AuthContext";
+import ListaTemas from "./components/tema/listatemas/ListaTemas";
+import FormTema from "./components/tema/formtema/FormTema";
+import DeletarTema from "./components/tema/deletartema/DeletarTema";
 
 function App() {
   return (
@@ -29,6 +32,10 @@ function App() {
               {/* Rota para a página Home */}
               <Route path="/cadastro" element={<Cadastro />} />{" "}
               {/* Rota para a página Home */}
+              <Route path="/temas" element={<ListaTemas />} />
+              <Route path="/cadastrartema" element={<FormTema />} />
+              <Route path="/editartema/:id" element={<FormTema />} />
+              <Route path="/deletartema/:id" element={<DeletarTema />} />
             </Routes>
           </div>
           <Footer />
--- src/services/Service.ts
+++ src/services/Service.ts
@@ -15,3 +15,26 @@ export const login = async (url: string, dados: Object, setDados: Function) => {
     const resposta = await api.post(url, dados)
     setDados(resposta.data)
 }
+
+//  responsável por executar todas as operações de busca de recursos na aplicação, como Temas, Postagens
+export const buscar = async (url: string, setDados: Function, header: Object) => {
+    const resposta = await api.get(url, header)
+    setDados(resposta.data)
+}
+
+//Cadastrar novos objetos 
+export const cadastrar = async (url: string, dados: Object, setDados: Function, header: Object) => {
+    const resposta = await api.post(url, dados, header)
+    setDados(resposta.data)
+}
+
+//Atualizar objetos existentes 
+export const atualizar = async (url: string, dados: Object, setDados: Function, header: Object) => {
+    const resposta = await api.put(url, dados, header)
+    setDados(resposta.data)
+}
+
+//deletar tema 
+export const deletar = async (url: string, header: Object) => {
+    await api.delete(url, header)
+}
