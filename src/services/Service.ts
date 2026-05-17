d99b3adf0e5e2ca395c71c05feb094fb32f82a41
Alice Tolosa <aliceoliveira81@outlook.com>
2026-05-17 16:17:29 -0300

    Deploy do Projeto Blog Pessoal

--- src/services/Service.ts
+++ src/services/Service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create ({
-    baseURL: "https://blogpessoal-nest-664p.onrender.com"
+    baseURL: import.meta.env.VITE_API_URL
 })
 
 //metodo de acesso a api para cadastrar usuario
